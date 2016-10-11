67fc3f700b0d1edbbed7e71398402dc87bbbb9a5
Ilsa Gordon <ilsalacious@gmail.com>
2016-10-11 23:46:00 +0000

    trying to connect database1

--- Model.py
+++ Model.py
@@ -1,5 +1,5 @@
 """Models for Slack TicTacToe Coding Challenge"""
-from flask import Flask
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -113,7 +113,7 @@ class Emoji(db.Model):
 # Database Helper functions
 
 def connect_to_db(app, db_uri=None):
-    '''Connect the database to our Flask app.'''
+    '''Connect the database to Flask app.'''
 
     app = Flask("app")
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri or 'postgres:///oxo'
