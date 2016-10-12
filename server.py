018585b7cbefc0d2a965cf23afce4c053d7fe9db
Ilsa Gordon <ilsalacious@gmail.com>
2016-10-12 18:19:21 +0000

    Testing the Flask-Slack extension

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, make_response
 
-# from flask_slack import Slack
+from flask_slack import Slack
 
 import os
 
@@ -20,13 +20,10 @@ app.secret_key = os.environ['APP_SECRET_KEY']
 
 slack_token = os.environ['SLACK_TOKEN']
 
-# slack = Slack(app)
-# app.add_url_rule('/game.json', view_func=slack.dispatch)
+slack = Slack(app)
+app.add_url_rule('/game.json', view_func=slack.dispatch)
 
 # ___________________________________________________________________________
-
-# show_help = {"text": "*How to play OXO Emoji Tic-Tac-Toe:*\n-Challenge anyone on your team to a game by entering `/ttt` and your opponents username in any channel, e.g. `/ttt kenny`. There is one game per channel, at a time.\n- By default, your moves will show as :heavy_multiplication_x: and your opponents moves will show as :O:.  To change the emoji that represents your moves, type `/ttt` and the emoji you would like to use, e.g. `/ttt :alien:`. If the emoji you chose has aleady been chosen by another player, your emoji will not be updated.\n- Anyone can type `/ttt` at any time to show the game board and whose turn it is, but only the person whose turn it is can make a move.\n- To make your move, type `/ttt` and the number of the square you would like to claim, e.g. `/ttt 1`.\n- To end the game prematurely, type `/ttt catsgame`\n- To see this message again,type `/ttt help`\nThis:\n\n:one:|:two:|:three:\n:four:|:five:|:six:\n:seven:|:eight:|:nine:\n\n Quickly turns into this:\n\n :ghost:|:alien:|:three:\n:four:|:ghost:|:six:\n:alien:|:alien:|:ghost:"}
-
 
 # @app.route('/test', methods=['GET'])
 # def test():
@@ -34,7 +31,7 @@ slack_token = os.environ['SLACK_TOKEN']
 
 #     html = "<html><body>Testing testing, is this thing on?</body></html>"
     #  Pretty silly, but this is how I loaded my seed data for the emojis
-    #  because I had trouble with the Heroku python CLI
+    #  because I was getting error messages from the Heroku python CLI
 
     # for row in (open("valid_emojis.txt")):
     #     row = row.rstrip()
@@ -44,121 +41,35 @@ slack_token = os.environ['SLACK_TOKEN']
 
     # return html
 
+# @app.route('/game.json', methods=['POST'])
+# def new_game():
+#     '''Processes slash commands to play Tic-Tac-Toe'''
 
-# @slack.command('/ttt', token= slack_token, team_id="T2LQ2JYJF", methods=['POST'])
-# def show_help(**kwargs):
-#     text = kwargs.get('text')
-#     return slack.response(text)
+#     data = request.form
+#     query_text = data['text']
+#     if query_text == "help":
 
-@app.route('/game.json', methods=['POST'])
-def new_game():
-    '''Processes slash commands to play Tic-Tac-Toe'''
+#         payload = {"text": "*How to play OXO Emoji Tic-Tac-Toe:*\n-Challenge anyone on your team to a game by entering `/ttt` and your opponents username in any channel, e.g. `/ttt kenny`. There is one game per channel, at a time.\n- By default, your moves will show as :heavy_multiplication_x: and your opponents moves will show as :O:.  To change the emoji that represents your moves, type `/ttt` and the emoji you would like to use, e.g. `/ttt :alien:`. If the emoji you chose has aleady been chosen by another player, your emoji will not be updated.\n- Anyone can type `/ttt` at any time to show the game board and whose turn it is, but only the person whose turn it is can make a move.\n- To make your move, type `/ttt` and the number of the square you would like to claim, e.g. `/ttt 1`.\n- To end the game prematurely, type `/ttt catsgame`\n- To see this message again,type `/ttt help`\nThis:\n\n:one:|:two:|:three:\n:four:|:five:|:six:\n:seven:|:eight:|:nine:\n\n Quickly turns into this:\n\n :ghost:|:alien:|:three:\n:four:|:ghost:|:six:\n:alien:|:alien:|:ghost:"}
 
-    data = request.form
-    query_text = data['text']
-    if query_text == "help":
+#         # dump that dictionary to json
+#         json_payload = json.dumps(payload)
 
-        payload = {"text": "*How to play OXO Emoji Tic-Tac-Toe:*\n-Challenge anyone on your team to a game by entering `/ttt` and your opponents username in any channel, e.g. `/ttt kenny`. There is one game per channel, at a time.\n- By default, your moves will show as :heavy_multiplication_x: and your opponents moves will show as :O:.  To change the emoji that represents your moves, type `/ttt` and the emoji you would like to use, e.g. `/ttt :alien:`. If the emoji you chose has aleady been chosen by another player, your emoji will not be updated.\n- Anyone can type `/ttt` at any time to show the game board and whose turn it is, but only the person whose turn it is can make a move.\n- To make your move, type `/ttt` and the number of the square you would like to claim, e.g. `/ttt 1`.\n- To end the game prematurely, type `/ttt catsgame`\n- To see this message again,type `/ttt help`\nThis:\n\n:one:|:two:|:three:\n:four:|:five:|:six:\n:seven:|:eight:|:nine:\n\n Quickly turns into this:\n\n :ghost:|:alien:|:three:\n:four:|:ghost:|:six:\n:alien:|:alien:|:ghost:"}
+#         # JSON response must have json header for Slack
+#         # update this to use flask.jsonify()
+#         # http://stackoverflow.com/questions/7907596/json-dumps-vs-flask-jsonify
+#         response = make_response(json_payload)
+#         response.headers['content-type'] = 'application/json'
 
-        # dump that dictionary to json
-        json_payload = json.dumps(payload)
+#         return response
+#     else:
+#         return 'I don\'t know that one. Enter \'/ttt help\' to see valid commands'
 
-        # JSON response must have json header for Slack
-        # update this to use flask.jsonify()
-        # http://stackoverflow.com/questions/7907596/json-dumps-vs-flask-jsonify
-        response = make_response(json_payload)
-        response.headers['content-type'] = 'application/json'
-
-        return response
-    else:
-        return 'I don\'t know that one'
-
-
-    # cheese = requests.get_json(force=True)
-    # stuff = open("json.txt", "w")
-    # stuff.write(cheese)
-    # stuff.close()
-
-
-
-    # token = request.data["token"]
-    # if token == slack_token:
-    #     text = request.data["text"]
-    #     if text == "help":
-    #         return jsonify(show_help)
-    #     team_domain = request.data["team_domain"]
-    #     domain = team_domain
-    #     channel_id = request.data["channel_id"]
-    #     channel_name = request.data["channel_name"]
-    #     user_id = request.data["user_id"]
-    #     user_name = request.data["user_name"]
-    #     if text[0] == "@":
-    #         domain = Game(player_one_user_name=user_name,
-    #                   player_two_user_name=text,
-    #                   channel_name=channel_name,
-    #                   player_one_user_id=user_id,
-    #                   )
-    # elif text[0] ==":":
-
-
-    #     print (token, text, team_domain,
-    #            team_domain,
-    #            domain,
-    #            channel_name,
-    #            user_id,
-    #            user_name,
-    #            command,
-    #            text,
-    #            response_url)
-
-    #     url = response_url
-    #     headers = {'Content-type': 'application/json'}
-    #     payload = {'text': 'Slash command recieved. Cats rule!'}
-    #     r = requests.post(url, headers=headers, data=payload)
-
-    #     print r
-
-    #     return jsonify(payload), status.HTTP_200_OK
-    # else:
-    #     payload = {'text': 'Not a valid token.'}
-    #     return jsonify(payload), status.HTTP_400_BAD_REQUEST
-
-    # token = "gIkuvaNzQIHg97ATvDxqgjtO"
-    # team_id = "T0001"
-    # team_domain = "example"
-    # channel_id = "C2147483705"
-    # channel_name = "test"
-    # user_id = "U2147483697"
-    # user_name = "Steve"
-    # command = "/weather"
-    # text = "94070"
-    # response_url = "https://hooks.slack.com/commands/1234/5678"
-
-        # execute the command
-
-        # If you'd like to add HTTP headers to a request, simply pass in a dict to the headers parameter.
-
-        # For example, we didn't specify our user-agent in the previous example:
-
-        # >>> url = 'https://api.github.com/some/endpoint'
-        # >>> headers = {'user-agent': 'my-app/0.0.1'}
-
-        # >>> r = requests.get(url, headers=headers)
-        # Note: Custom headers are given less precedence than more specific sources of information. For instance:
-
-         # r = requests.post('http://httpbin.org/post', data = {'key':'value'})'
-
-         # http://flask.pocoo.org/docs/0.11/quickstart/#about-responses
-
-    #     pass
-
-    # else:
-    #     # error message or pass. The request did not come from Slack.
-    #     # "Additionally, you can verify whether the team_id matches a
-    #     # team that has approved your command for usage.
-    #     # If the token or team are unknown to your application, you should
-    #     # refuse to service the request and return an error instead."
-    #     pass
+@slack.command('/ttt', token=slack_token, team_id='team_id": "T2LQ2JYJF',
+               methods=['POST', 'GET'])
+def new_game(**kwargs):
+    text = kwargs.get('text')
+    saying = {"text": "Testing to see if this other method is working. The text that was sent is %s" % text}
+    return slack.response(saying)
 
 # ___________________________________________________________________________
 
